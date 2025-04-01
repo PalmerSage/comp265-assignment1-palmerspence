78a2ee972ab275addf20ed6f074dbe183b7d9b45
Palmer Spence <palmerspence@gmail.com>
2025-03-31 18:06:28 -0600

    Add first emoji box with emoji (no interactivity yet)

--- App.js
+++ App.js
@@ -5,19 +5,44 @@ const App = () => {
   return (
     <ScrollView contentContainerStyle={styles.container}>
       <Text style={styles.title}>Palmoji</Text>
+
+      <View style={styles.emojiBox}>
+        <Text style={styles.emoji}>😊</Text>
+      </View>
     </ScrollView>
+    
+  
   );
 };
 
 const styles = StyleSheet.create({
+
   container: {
     alignItems: 'center',
     paddingVertical: 40,
   },
+  // App Title obvi
   title: {
     fontSize: 32,
     fontWeight: 'bold',
   },
+  // Box with emoji in it
+  emojiBox: {
+    width: 80,
+    height: 80,
+    borderWidth: 1,
+    borderColor: '#000',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 20,
+  },
+  // Emoji size
+  emoji: {
+    fontSize: 32,
+  },
+
+
+
 });
 
 export default App;
